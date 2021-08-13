5e364997148ece9012d96a25479e420e0e7454e1
sggongshow <87620031+sggongshow@users.noreply.github.com>
2021-08-13 15:41:38 -0700

    Create ticklerEfficiency.user.js

---
+++ ticklerEfficiency.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name        Tickler Efficiency
+// @namespace   GongOscar
+// @description Constant EForm Submit and Print button locations
+// @include     */ticklerAdd.jsp?*
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
+// @version 15.2
+// ==/UserScript==
+
+
+//wait window load first
+window.addEventListener('load', function() {
+  var textBox = $('textarea[name="textarea"]')
+  textBox.select()  
+}, false);
+
+
+document.addEventListener('keydown', function(theEvent) {
+	var theKey = theEvent.key
+	var theAltKey =theEvent.altKey;
+	var theCtrlKey = theEvent.ctrlKey;
+	var theShiftKey= theEvent.shiftKey;
+  
+  
+  switch(true){
+      //Acknowledge  button
+    case theAltKey && theKey==='1': 
+      var subButton = $('input[type="button"][value*=Submit]')
+      subButton.click()
+			break;
+      
+    default:
+      break; 
+  }
+  
+  
+}, true);
+
