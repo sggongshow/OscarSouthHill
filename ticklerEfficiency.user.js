203ff66c05654270d91c4ceb03c7d7e373cd6c4a
sggongshow <87620031+sggongshow@users.noreply.github.com>
2021-08-16 12:17:41 -0700

    Update ticklerEfficiency.user.js

--- ticklerEfficiency.user.js
+++ ticklerEfficiency.user.js
@@ -3,6 +3,7 @@
 // @namespace   GongOscar
 // @description Constant EForm Submit and Print button locations
 // @include     */ticklerAdd.jsp?*
+// @include     *ForwardDemographicTickler.do?*
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
 // @version 15.2
 // ==/UserScript==
@@ -25,7 +26,7 @@ document.addEventListener('keydown', function(theEvent) {
   switch(true){
       //Acknowledge  button
     case theAltKey && theKey==='1': 
-      var subButton = $('input[type="button"][value*=Submit]')
+      var subButton = $('input[type="button"][value*=Submit][value*=EXIT]')
       subButton.click()
 			break;
       
