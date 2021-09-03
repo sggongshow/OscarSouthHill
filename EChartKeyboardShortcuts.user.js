97bdbdc225c2625ff6e00cdb73249baca463dd42
sggongshow <87620031+sggongshow@users.noreply.github.com>
2021-09-03 15:04:29 -0700

    Update EChartKeyboardShortcuts.user.js

--- EChartKeyboardShortcuts.user.js
+++ EChartKeyboardShortcuts.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           EChart_KeyboardShortcuts
-// @namespace      oscar/StansScripts
+// @namespace      GongOscar
 // @version 15.2
 // @description Various Echart shortcut buttons (Alt+ e,v,z,x,k,s,0,1,2,3,4,5,7). Set your own Measurement groupName and default population text.
 // @include        */casemgmt/forward.jsp?action=view&demographic*
