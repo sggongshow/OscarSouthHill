4f5f8a9c4d072efe8069a2b1422c6123e03cb322
sggongshow <87620031+sggongshow@users.noreply.github.com>
2022-02-28 21:23:23 -0800

    Update BillingCodeToText.user.js

--- BillingCodeToText.user.js
+++ BillingCodeToText.user.js
@@ -9,7 +9,9 @@
 // @include     *SaveBilling.do?*
 // @include     *formwcb.do?*
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
-// @version 15.2
+// @updateURL https://github.com/sggongshow/OscarSouthHill/raw/main/BillingCodeToText.user.js
+// @downloadURL https://github.com/sggongshow/OscarSouthHill/raw/main/BillingCodeToText.user.js
+// @version 21.02.28.5
 // ==/UserScript==
 
 
@@ -23,19 +25,43 @@ window.addEventListener('load', function() {
 
 function main(){
 
-  var codeList = $('a[href*=myFunction][onclick*=PickD]')
-  console.log(codeList)
+  ///change the names of recently used to text instead of ICD9
+  var codeList = $('#DX_REFERENCE')[0].querySelector('.form-control')
+  //console.log(codeList)
 
 	for (let i = 0; i < codeList.length; i++) {
   	
     var title = codeList[i].title
     var dxCode = codeList[i].text.trim()
 
-    codeList[i].text = "- " + title + dxCode
-		codeList[i].title = dxCode
+    codeList[i].text = "-" + title + dxCode
+		//codeList[i].title = dxCode
 	}
+  
+  //smaller font so more ICD shows up
+  codeList.style.fontSize = '10px'
+  codeList.style.height = '200px'
+
+  //Re-organize the billing page structure so that the recently used is no a separate row
+  var CodePosition = $('[class = "table table-condensed table-borderless"]')
+  var table = CodePosition[2]
+  var ICDBox = $('#DX_REFERENCE').parent()[0]
+  var headingTr = ICDBox.parentElement.previousElementSibling
+  
+  
+  //console.log(table)
+  //console.log(ICDBox)
+  //console.log(headingTr)
+  //headingTr.children[1].remove()
+  table.insertBefore(ICDBox,null)
+  ICDBox.insertBefore(headingTr.children[1],null)
+  
+  
+  
+  
+  
 
   
-  
+  // $("tbody tr:nth-child(11)").insertAfter("tbody tr:nth-child(4)");
 
 }
